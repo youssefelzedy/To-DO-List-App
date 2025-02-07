875d8b4e075d068b2f716815b46837ce3eea0c20
Youssef Elzedy <youssefelzedy90@gmail.com>
2025-02-07 15:19:04 +0200

    Implement project management features with drag and drop functionality, including project state management, validation, and input handling.

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,13 @@
+namespace App {
+  export function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const originalMethood = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethood.bind(this);
+        return boundFn;
+      },
+    };
+    return adjDescriptor;
+  }
+}
